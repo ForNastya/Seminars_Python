f6c6b96e2a8ee8d610f4a60c110d14100cf02a9b
Anastasyia <moya_pochta_n.k@mail.ru>
2023-02-04 14:58:43 +0700

    Task_02

--- Seminar_8/Task_01.py
+++ Seminar_8/Task_01.py
@@ -1,2 +1,37 @@
-def decrypt_caesar(msg, shift=3):
-    return encrypt_caesar(msg, -1 * shift)
+def choose_coffee(*preference):
+    global ingredients
+    for pr in preference:
+        if pr == 'Эспрессо':
+            if ingredients[0] >= 1:
+                ingredients[0] -= 1
+                return pr
+        if pr == 'Капучино':
+            if ingredients[0] >= 1 and ingredients[1] >= 3:
+                ingredients[0] -= 1
+                ingredients[1] -= 3
+                return pr
+        if pr == 'Маккиато':
+            if ingredients[0] >= 2 and ingredients[1] >= 1:
+                ingredients[0] -= 2
+                ingredients[1] -= 1
+                return pref
+        if pr == 'Кофе по-венски':
+            if ingredients[0] >= 1 and ingredients[2] >= 2:
+                ingredients[0] -= 1
+                ingredients[2] -= 2
+                return pr
+        if pr == 'Латте Маккиато':
+            if ingredients[0] >= 1 and ingredients[1] >= 2 and ingredients[2] >= 1:
+                ingredients[0] -= 1
+                ingredients[1] -= 2
+                ingredients[2] -= 1
+                return pr
+        if pr == 'Кон Панна':
+            if ingredients[0] >= 1 and ingredients[2] >= 1:
+                ingredients[0] -= 1
+                ingredients[2] -= 1
+                return pr
+    return 'К сожалению, не можем предложить Вам напиток'
+
+ingredients = [2, 1, 0]
+print(choose_coffee('Капучино','Макиато','Эспрессо'))
